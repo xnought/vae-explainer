33b05b66e4d4fb2896ddfcd6b474ee643a193d41
xnought <dbertucci6@gatech.edu>
2024-09-02 18:29:21 -0400

    load model into js

--- website/src/lib/load.js
+++ website/src/lib/load.js
@@ -1,17 +1,27 @@
 import * as tf from "@tensorflow/tfjs";
 
-tf.tidy(() => {
-	const model = tf.loadGraphModel("/models/tfjs/encoder/model.json");
-	model.then((d) => {
-		const out = d.predict(tf.ones([1, 784]));
-		console.log(tf.sum(out).toString());
-	});
-});
+function sample(code) {
+	const [zLogVar, zMean] = tf.split(code, 2, -1); // since the dense(2*latent) gives x[start:half] as log vars and x[half:end] as means
+	const zStdDev = tf.exp(zLogVar.mul(0.5)); // e^(1/2 sigma^2) = sigma
+
+	const eps = tf.randomStandardNormal(zMean.shape);
+	const z = tf.add(zMean, tf.mul(eps, zStdDev)); // \mu + \sigma * \epsilon    or stddev*N(0, I) + mean
+
+	return [z, zLogVar, zMean];
+}
+
+async function loadModels() {
+	const encoder = await tf.loadGraphModel("/models/tfjs/encoder/model.json");
+	const decoder = await tf.loadGraphModel("/models/tfjs/decoder/model.json");
+	return [encoder, decoder];
+}
 
 tf.tidy(() => {
-	const model = tf.loadGraphModel("/models/tfjs/decoder/model.json");
-	model.then((d) => {
-		const out = d.predict(tf.ones([1, 2]));
-		console.log(out.sum().toString());
+	loadModels().then(([enc, dec]) => {
+		const input = tf.ones([1, 784]);
+		const encoded = enc.predict(input);
+		const [z, zLogVar, zMean] = sample(encoded);
+		console.log(z.toString(), zLogVar.toString(), zMean.toString());
+		const decoded = dec.predict(z);
 	});
 });
