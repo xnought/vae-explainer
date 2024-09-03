5eb48f6c39ab134091ff542efebdd8991968ed71
xnought <dbertucci6@gatech.edu>
2024-09-03 16:29:11 -0400

    feat: select and pass through data

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -1,11 +1,13 @@
 <script>
-	import { onMount } from "svelte";
+	import { onDestroy, onMount } from "svelte";
 	import { loadImage, loadModels, sample } from "./lib/load";
+	import { Button } from "flowbite-svelte";
 	import * as tf from "@tensorflow/tfjs";
 	import MnistDigit from "./lib/digit/MnistDigit.svelte";
 	import NormalCurve from "./lib/NormalCurve.svelte";
 	import LatentScatter from "./lib/LatentScatter.svelte";
 	import Header from "./lib/Header.svelte";
+	import ImageSelector from "./lib/ImageSelector.svelte";
 
 	function toGrey(d) {
 		const result = new Uint8ClampedArray(d.length / 4);
@@ -19,20 +21,28 @@
 		console.table(tf.memory());
 	}
 
+	async function loadImageFull(url) {
+		const img = await loadImage(url);
+		const canvas = document.createElement("canvas");
+		const ctx = canvas.getContext("2d");
+		ctx.drawImage(img, 0, 0);
+		const d = ctx.getImageData(0, 0, img.width, img.height).data;
+		img.remove();
+		canvas.remove();
+		return d;
+	}
+
+	const images = [1, 2, 3, 4, 5, 7].map((d) => `images/${d}.png`);
+	let selectedImage = "images/1.png";
 	const latentDims = 2;
-	let canvas;
 	let inDisp = Array(784).fill(0);
 	let outDisp = Array(784).fill(0);
 	let stddevs = Array(latentDims).fill(1);
 	let means = Array(latentDims).fill(0);
 	let zs = Array(latentDims).fill(0);
-	onMount(async () => {
-		const [enc, dec] = await loadModels();
-		const img = await loadImage("/images/5.png");
-		const ctx = canvas.getContext("2d");
-		ctx.drawImage(img, 0, 0);
-		const d = ctx.getImageData(0, 0, img.width, img.height).data;
 
+	async function forward(url) {
+		const d = await loadImageFull(url);
 		tf.tidy(() => {
 			const x = tf.tensor(toGrey(d), [1, 784]).div(255.0);
 			inDisp = x.arraySync()[0];
@@ -47,29 +57,37 @@
 			const xHat = dec.predict(z);
 			outDisp = xHat.arraySync()[0];
 		});
+	}
 
+	$: modelsExist = enc && dec;
+	$: if (modelsExist) forward(selectedImage);
+
+	let enc, dec;
+	onMount(async () => {
+		[enc, dec] = await loadModels();
+	});
+	onDestroy(() => {
 		enc.dispose();
 		dec.dispose();
-
-		showMemory();
 	});
 </script>
 
 <Header></Header>
 <main>
-	<canvas bind:this={canvas} width={28} height={28}> </canvas>
-
+	<div class="mb-2">
+		<ImageSelector imageUrls={images} bind:selectedUrl={selectedImage} />
+	</div>
 	<MnistDigit data={inDisp} square={250} maxVal={1}></MnistDigit>
-	{#each means as mean, i}
+	<!-- {#each means as mean, i}
 		{@const stddev = stddevs[i]}
 		{@const z = zs[i]}
 		<div>
 			<NormalCurve x={z} {mean} {stddev}></NormalCurve>
 		</div>
-	{/each}
+	{/each} -->
+	<!-- <LatentScatter></LatentScatter> -->
 	<MnistDigit data={outDisp} square={250} maxVal={1}></MnistDigit>
-
-	<LatentScatter></LatentScatter>
+	<Button on:click={() => showMemory()}>tf.memory()</Button>
 </main>
 
 <style>
---
+++ website/src/lib/ImageSelector.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let imageUrls = [];
+	export let selectedUrl;
+</script>
+
+<div class="flex gap-2">
+	{#each imageUrls as src, i}
+		<img
+			{src}
+			alt="begone yellow squigly lines"
+			width={40}
+			class:lined={selectedUrl === src}
+			on:click={() => {
+				selectedUrl = src;
+			}}
+		/>
+	{/each}
+</div>
+
+<style>
+	.lined {
+		outline: 2px solid var(--pink);
+		opacity: 1;
+	}
+	img {
+		cursor: pointer;
+		border-radius: 3px;
+		opacity: 0.4;
+	}
+</style>
