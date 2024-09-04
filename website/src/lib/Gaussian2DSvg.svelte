5476f1228aa7beac671407f7ea41e25a70fe88c6
xnought <dbertucci6@gatech.edu>
2024-09-04 17:03:01 -0400

    fixed width of gaussians

--- website/src/lib/Gaussian2DSvg.svelte
+++ website/src/lib/Gaussian2DSvg.svelte
@@ -11,8 +11,8 @@
 	<ellipse
 		cx={scaleX(means[0])}
 		cy={scaleY(means[1])}
-		rx={exageration * 2 * stddevs[0] * (numberOfDeviations - i)}
-		ry={exageration * 2 * stddevs[1] * (numberOfDeviations - i)}
+		rx={exageration * stddevs[0] * (numberOfDeviations - i)}
+		ry={exageration * stddevs[1] * (numberOfDeviations - i)}
 		fill="var(--pink)"
 		opacity={0.1 * i}
 		stroke-width={1}
---
+++ website/src/lib/Popover.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let width = 800;
+  export let height = 500;
+</script>
+
+<div>
+  <svg {width} {height}>
+    <rect x={0} y={0} {width} {height} fill="black"/>
+  </svg>
+</div>
+
