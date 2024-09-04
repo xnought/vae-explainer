1675e45b99735ede1178ca5a148db059b0fc0c5e
xnought <dbertucci6@gatech.edu>
2024-09-04 19:53:51 -0400

    log var trick

--- website/src/lib/LogVarTrick.svelte
+++ website/src/lib/LogVarTrick.svelte
@@ -12,8 +12,8 @@
   const halfExp = (x) => Math.exp(0.5*x);
   const expSquare = 40;
 
-  const firstExpX = 15;
-  const secondExpX = 15;
+  export const logVarVectorX = 15;
+  export const firstExpX = logVarVectorX + 50;
 
   const firstExpY = 15;
   const secondExpY = firstExpY + 55;
@@ -22,20 +22,40 @@
 
   const outConnectStroke = {stroke: "grey", "stroke-width": 2};
 
-  const middleFirstExp = firstExpY + expSquare/2;
-  const middleSecondExp = secondExpY + expSquare/2;
+  export const middleFirstExp = firstExpY + expSquare/2;
+  export const middleSecondExp = secondExpY + expSquare/2;
+
+  let vectorN1, vectorN2;
 </script>
 
 <svg {x} {y} {width} {height}>
   <rect {x} {y} {width} {height} fill="none" stroke="black"/>
 
+  <!--log(var) -->
+  <VectorShape x={logVarVectorX}
+               y={firstExpY} 
+               height={secondExpY - firstExpY + expSquare} 
+               bind:n1={vectorN1} 
+               bind:n2={vectorN2} 
+               values={logVars.map(halfExp)}
+               stroke="pink"
+  />
+
+  {#if vectorN1 && vectorN2}
+    <line x1={logVarVectorX + 30} y1={middleFirstExp} x2={firstExpX} y2={middleFirstExp} {...outConnectStroke}/>
+    <line x1={logVarVectorX  + 30} y1={middleSecondExp} x2={firstExpX} y2={middleSecondExp} {...outConnectStroke}/>
+  {/if}
+
   <FunctionSVG x={firstExpX} y={firstExpY} width={expSquare} height={expSquare} f={halfExp} input={logVars[0]}/>
   <FunctionSVG x={firstExpX} y={secondExpY} width={expSquare} height={expSquare} f={halfExp} input={logVars[1]}/>
 
-  <line x1={firstExpX + expSquare} y1={middleFirstExp} x2={vectorX} y2={middleFirstExp} {...outConnectStroke}/>
-  <line x1={secondExpX + expSquare} y1={middleSecondExp} x2={vectorX} y2={middleSecondExp} {...outConnectStroke}/>
+  {#if vectorN1 && vectorN2}
+    <line x1={firstExpX + expSquare} y1={middleFirstExp} x2={vectorX} y2={vectorN1+firstExpY} {...outConnectStroke}/>
+    <line x1={firstExpX + expSquare} y1={middleSecondExp} x2={vectorX} y2={vectorN2+firstExpY} {...outConnectStroke}/>
+  {/if}
 
-  <VectorShape x={vectorX} y={firstExpY} height={secondExpY - firstExpY + expSquare}/>
+  <!--sigma -->
+  <VectorShape x={vectorX} y={firstExpY} height={secondExpY - firstExpY + expSquare} bind:n1={vectorN1} bind:n2={vectorN2} values={logVars.map(halfExp)}/>
 </svg>
 
 <style>
--- website/src/lib/VectorShape.svelte
+++ website/src/lib/VectorShape.svelte
@@ -1,9 +1,32 @@
 <script>
+  import * as d3 from "d3";
+
   export let x;
   export let y;
-  export let width = 15; 
+  export let width = 30; 
   export let height =100;
-  export let fill = "steelblue";
+  export let stroke = "rgb(144,220,147)";
+  export let fillOpacity = 0.2;
+  export let strokeWidth = 3;
+
+  export let values = [0, 0];
+  export const n1 = 20;
+  export const n2 = 75;
+
+  $: fill = d3.color(stroke);
+  $: fill.opacity = fillOpacity;
 </script>
 
-<rect {x} {y} {height} {width}/>
+<svg {width} {height} {x} {y}>
+  <rect x={0} y={0} {height} {width} {stroke} stroke-width={strokeWidth} fill={fill.toString()}/>
+  <text text-anchor="middle" x={width/2} y={n1 + 4}>{values[0].toFixed(1)}</text>
+  <text text-anchor="middle" x={width/2} y={n2 + 4}>{values[1].toFixed(1)}</text>
+</svg>
+
+
+<style>
+  text {
+    font-size: 10px;
+    font-family: menlo;
+  }
+</style>
